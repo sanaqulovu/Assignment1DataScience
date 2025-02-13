a7f88e12163ceee0ddbda962e920329d448fb421
sanaqulovu <161562072+sanaqulovu@users.noreply.github.com>
2025-02-13 20:55:20 +0500

    Update task3.py
    task 3 finished

--- src/task3.py
+++ src/task3.py
@@ -1,1 +1,11 @@
+import time
+import contextlib
 
+def decorator_1(f):
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        with contextlib.redirect_stdout(None):
+            f(*args, **kwargs)
+        end = time.time()
+        print(f"{f.__name__} call executed in {end - start : .7f} sec")
+    return wrapper
